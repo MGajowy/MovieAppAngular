69bba4f07afdf39bcebcf7e0455249bccce224b0
michal.gajewski <michalpiotr.gajewski@gmail.com>
2023-11-29 09:58:45 +0100

    update3

--- src/app/movie/components/modify-movie/modify-movie.component.ts
+++ src/app/movie/components/modify-movie/modify-movie.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from '../../shared/Movie';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MoviesService } from '../../services/movies.service';
+import { Director } from 'src/app/director/shared/Director';
+import { DirectorsService } from 'src/app/director/services/directors.service';
 
 @Component({
   selector: 'app-modify-movie',
@@ -7,9 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModifyMovieComponent implements OnInit {
 
-  constructor() { }
+  movie: Movie;
+  id: number;
+  selectedDirector: Director;
+  directorList: Director[] = [];
+
+  constructor(
+    private moviesService: MoviesService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private directorServieces: DirectorsService,
+  ) { }
 
   ngOnInit() {
+    this.getDirectorList();
+    this.id = this.route.snapshot.params['id'];
+    this.moviesService.getMovie(this.id).subscribe(result => {
+      this.movie = result;
+    })
+  }
+
+  updateMovie(){
+    this.movie.directorId = this.selectedDirector.id;
+
+    this.moviesService.updateMovie(this.id, this.movie).subscribe(result => {
+      console.log('OK')
+      this.router.navigate(['movies']);
+    } )
+  }
+
+  getDirectorList() {
+    this.directorServieces.getAllDirector().subscribe(result => {
+      this.directorList = result;
+    })
   }
 
 }
